270a85fd54102c190a11eac070d0c9cc7de3b331
rarangur <rarangur@student.42.fr>
2025-05-21 14:13:08 +0100

    fix ft_bzero ft_lstmap

--- libft/src/ft_lstmap.c
+++ libft/src/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: bduval <bduval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 12:16:19 by bduval            #+#    #+#             */
-/*   Updated: 2024/11/19 12:30:14 by bduval           ###   ########.fr       */
+/*   Updated: 2025/05/21 14:09:04 by rarangur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ static t_list	*ft_lstnewf(void *content, void *(*f)(void *))
 	t_list	*l;
 
 	l = ft_lstnew(content);
-	l->content = f(l->content);
+	if (l)
+		l->content = f(l->content);
 	return (l);
 }
 
@@ -25,22 +26,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*start;
 	t_list	*current;
-	t_list	*temp;
 
 	start = NULL;
-	temp = NULL;
 	while (lst)
 	{
 		current = ft_lstnewf(lst->content, f);
-		if (temp)
-			temp->next = current;
-		if (start == NULL)
-			start = current;
-		temp = lst;
+		if (!current)
+		{
+			ft_lstclear(&start, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&start, current);
 		lst = lst->next;
-		del(temp);
-		temp = current;
-		current = current->next;
 	}
 	return (start);
 }
