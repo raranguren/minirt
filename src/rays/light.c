3a9f76f158195dfd3a1482c16f35d704b3c230a1
1Batmain <batduval@gmail.com>
2025-06-04 21:48:06 +0000

    multi_light_fix

--- src/rays/light.c
+++ src/rays/light.c
@@ -6,7 +6,7 @@
 /*   By: bduval <bduval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 16:31:55 by bduval            #+#    #+#             */
-/*   Updated: 2025/06/04 17:15:56 by bduval           ###   ########.fr       */
+/*   Updated: 2025/06/04 21:47:08 by bduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,17 +34,21 @@ int	set_ray_to_impact(t_ray *ray)
 
 int	spotlight(t_light *light, t_ray *ray)
 {
-	ray->direct_light = c_scale(
-			c_multiply(
-				ray->direct_light,
-				c_scale(c_normalize(light->color), light->brightness)),
-			fmax(v_dot(ray->normal, ray->direction), 0));
-	ray->specular_light = c_scale(
-			c_multiply(
-				ray->specular_light,
-				c_scale(c_normalize(light->color), light->brightness)),
-			pow(fmax(v_dot(ray->normal, ray->direction), 0), REFRACT));
-	ray->color = c_multiply(ray->impact_object->color, ray->direct_light);
+	ray->direct_light = c_multiply(
+			ray->direct_light, 
+			c_scale(c_multiply(
+					ray->direct_light,
+					c_scale(c_normalize(light->color), light->brightness)),
+				fmax(v_dot(ray->normal, ray->direction), 0)));
+	ray->specular_light = c_multiply(
+			ray->specular_light,
+				c_scale(c_multiply(
+					ray->specular_light,
+					c_scale(c_normalize(light->color), light->brightness)),
+				pow(fmax(v_dot(ray->normal, ray->direction), 0), REFRACT)));
+	ray->color = c_add(
+			ray->color, 
+			c_multiply(ray->impact_object->color, ray->direct_light));
 	ray->color = c_add(
 			ray->color,
 			c_multiply(light->color, ray->specular_light));
@@ -74,10 +78,8 @@ int	reach_spotlight(t_scene *scene, t_light *light, t_ray *ray)
 	if (v_dot(ray->normal, ray->bump) >= 0
 		|| v_dot(ray->normal,
 			v_unit(v_substract(light->pos, ray->start))) < 0)
-	{
 		if (!get_impact(scene, ray))
 			return (1);
-	}
 	return (0);
 }
 
