278907f2bebb7ae2a80380da921101a84b517bce
1Batmain <batduval@gmail.com>
2025-06-20 12:29:43 +0000

    ca_bump

--- src/rays/cylinder_bumb_bonus.c
+++ src/rays/cylinder_bumb_bonus.c
@@ -6,12 +6,32 @@
 /*   By: bduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 17:08:52 by bduval            #+#    #+#             */
-/*   Updated: 2025/06/20 10:51:11 by bduval           ###   ########.fr       */
+/*   Updated: 2025/06/20 12:28:29 by bduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+static t_vector	get_cyl_radial_coordinates(t_point p, t_obj *obj)
+{
+	t_vector uv;
+
+	uv.x = 0.5 + atan2f(p.y, p.x) / (2.0 * M_PI);
+	uv.y = (p.z + obj->height / 2.0) / obj->height;
+	uv.z = 0;
+	return (uv);
+}
+
+t_vector	get_disc_coordinates(t_point p_loc, t_obj *obj)
+{
+	t_vector	uv;
+	
+	uv.x = p_loc.x / (2.0 * obj->radius) + 0.5;
+	uv.y = p_loc.y / (2.0 * obj->radius) + 0.5;
+	uv.z = 0;
+
+	return (uv);
+}
 t_vector	cylinder_bump(t_ray *ray)
 {
 	t_point	p_loc;
@@ -22,16 +42,10 @@ t_vector	cylinder_bump(t_ray *ray)
 	p_loc = get_local_cordinates(ray->start, ray->impact_object->orientation,
 			ray->impact_object->pos);
 	if (fabs(proj_on_axis(ray->impact_object, ray->start)) < ray->impact_object->height / 2.0 - EPSLN)
-	{
-		p_rad.x = 0.5 + (atan2f(p_loc.y, p_loc.x) / (2 * M_PI));
-		p_rad.y = (p_loc.z + ray->impact_object->height / 2.0) / ray->impact_object->height;
-		p_rad.z = 0;
-		modifyer = get_bump_vector(&ray->impact_object->bump, p_rad);
-		ray->normal = add_in_tbn(ray->normal, modifyer);
-	}
+		p_rad = get_cyl_radial_coordinates(p_loc, ray->impact_object);
 	else
-	{
-		//caps
-	}
+		p_rad = get_disc_coordinates(p_loc, ray->impact_object);
+	modifyer = get_bump_vector(&ray->impact_object->bump, p_rad);
+	ray->normal = add_in_tbn(ray->normal, modifyer);
 	return (ray->normal);
 }
